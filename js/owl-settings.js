a72957a6e86e3723255beb74b33c437158eb6fb4
Elena <32584937+elena-kanakova@users.noreply.github.com>
2017-11-09 18:59:16 +0300

    Add files via upload
    Добавлены настройки для слайдеров.

--- js/owl-settings.js
+++ js/owl-settings.js
@@ -1,24 +1,68 @@
 $(document).ready(function() {
-	$('#').owlCarousel({
+	$('.slider').owlCarousel({
 		margin:0, //Отступ от картино если выводите больше 1
-		nav:true, //Отключил навигацию
-		autoplay:true, //Автозапуск слайдера        
+		nav:false, //Отключил навигацию
+		autoplay:true, //Автозапуск слайдера     
+        autoplaySpeed: 10,
         itemElement:'div',
         navClass:[ 'btn-gallery-left', 'btn-gallery-right'],
         navText: false,
         dots: true,
         dotsContainer: '.upper-slider-control',
-        dotsClass: '.upper-slider-dots',
-        dotClass: 'upper-slider-control', 
+        dotsClass: '.screen-1-slider-dots',
+        dotClass: '.upper-slider-control', 
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
         responsive:{
         0:{
-            items:1
+            items:1,
         },
-        600:{
-            items:1
+        451:{
+            items:1,
+        }
+    }
+	});
+});
+
+$(document).ready(function() {
+    
+    $('.tab-content').owlCarousel({
+		margin:0, //Отступ от картино если выводите больше 1
+		nav:true, //Отключил навигацию
+		autoplay:true,
+        itemElement:'div',
+        navClass:[ 'screen-7-slider-arrow-left', 'screen-7-slider-arrow-right'],
+        navText: false,
+        dots: false, 
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+        responsive:{
+        0:{
+            items:1,
         },
-        1000:{
-            items:1
+        451:{
+            items:1,
+        }
+    }
+	});
+});
+
+$(document).ready(function() {
+    
+    $('.screen-8-slider').owlCarousel({
+		margin:0, //Отступ от картино если выводите больше 1
+		nav:true, //Отключил навигацию
+		autoplay:true, //Автозапуск слайдера
+        itemElement:'div',
+        navClass:[ 'screen-8-slider-arrow-left', 'screen-8-slider-arrow-right'],
+        navText: false,
+        dots: false,
+        responsive:{
+        0:{
+            items:1,
+        },
+        451:{
+            items:3,
         }
     }
 	});
